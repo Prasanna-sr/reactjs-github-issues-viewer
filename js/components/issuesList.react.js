a9299cf535bb91cc01aff45949b6672515ddff78
prasanna ramanujam <prasannaraajkumar@gmail.com>
2016-01-07 13:19:56 -0800

    mock list generated

--- js/components/issuesList.react.js
+++ js/components/issuesList.react.js
@@ -1,11 +1,35 @@
 var React = require('react');
-var IssuesList = React.createClass({
-    render: function() {
-        return(
-            <div>
-                <ul><li> test2 </li> </ul>
+var Issues = React.createClass({
+   render: function() {
+      return (
+         <li>
+            <img/>
+            <div className="heading">
+               <label className="name">name
+                  <span>label</span>
+               </label>
+               <label className="title">title
+                  <span>no</span>
+               </label>
             </div>
-        );
-    }
+            <label className="summary">summary</label>
+         </li>
+      );
+   }
+});
+
+var IssuesList = React.createClass({
+   render: function() {
+      var list = [0, 1, 2, 4].map(function() {
+         return (<Issues/>);
+      });
+      return (
+          <div>
+              <ul>
+                  {list}
+              </ul>
+          </div>
+      )
+   }
 });
 module.exports = IssuesList;
